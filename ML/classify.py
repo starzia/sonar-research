755556ea9a39ffaa27c68c12c37682239ccf5e95
Steve Tarzia <steve@stevetarzia.com>
2009-03-02 18:48:22 +0000

    organizational changes. untested.

--- ML/classify.py
+++ ML/classify.py
@@ -102,7 +102,8 @@ def svm_train( training_data,
     return True
 
 
-def svm_test( test_data, model_file="/tmp/svm.model", in_file="/tmp/svm.in" ):
+def svm_test( test_data, model_file="/tmp/svm.model", in_file="/tmp/svm.in",
+              out_file="/tmp/svm.out" ):
     """data[ pos/neg, sample_index, data ]
     returns the accuracy.
     model_file is an input and in_file is a debugging output."""
@@ -112,7 +113,8 @@ def svm_test( test_data, model_file="/tmp/svm.model", in_file="/tmp/svm.in" ):
 
     # test classification model
     svm_light_format( test_data, in_file )
-    p= Popen("svm_classify %s %s" %(in_file,model_file),shell=True,stdout=PIPE)
+    p= Popen("svm_classify %s %s" %(in_file,model_file,out_file),
+             shell=True,stdout=PIPE)
     # kill the process if no results in 5 seconds
     Timer( 5, my_kill, [p.pid] ).start()
     output = p.communicate()[0]
@@ -120,11 +122,12 @@ def svm_test( test_data, model_file="/tmp/svm.model", in_file="/tmp/svm.in" ):
     match = re.search( "Accuracy on test set: (.*)%", output )
     if match:
         accuracy = float( match.group(1) ) / 100.0
-        # below, divide by 2 b/c we have an equal number of negative examples
+        f = open( out_file, 'r' )
+        results = f.readlines() # this will be a float for each test vec
+        f.close()
+        return [ accuracy, results ]
     else:
-        accuracy = -0.000001 # store nonsense value if svm failed
-    #print "classification success for model on new data was %d percent" % (100.0*accuracy)
-    return accuracy
+        return [ -0.000001, [] ] # store nonsense value if svm failed
 
 
 # DEFINE PARAMETERS :
@@ -132,23 +135,43 @@ def svm_test( test_data, model_file="/tmp/svm.model", in_file="/tmp/svm.in" ):
 # we will run clasification for each user plus for a combination of all:
 users = range( 20 )
 users.append( "all-users" )
+# we will be comparing each state to all other states
+states = ["typing","video","phone","puzzle","absent"]
+# we look at both the time-domain sample sequence and a freq-domain rep.:
+domains = ["time", "freq"]
 # we break the time-series data into this many samples (windows):
 samples = [10,100]
 # the data is modified by these mathematical operations:
 scalers = ["none","log","exp","square","sqrt"]
-# we look at both the time-domain sample sequence and a freq-domain rep.:
-domains = ["time", "freq"]
-# we will be comparing each state to all other states
-states = ["typing","video","phone","puzzle","absent"]
 # several Machine Learning approaches are tried:
 ##methods = ["svm","neural net"]
 methods = ["svm"]
+
+all_params = [ domains, samples, scalers, methods ]
+all_params_dims = []
+for i in all_params: all_params_dims.append( len(i) )
+
+
+def flatten_users( data ):
+    """data[user,state,time] is a numpy array
+    combines data from all users into one super-user"""
+    # collapse users axis:
+    shape = data.shape
+    new_data = data.reshape( ( 1, shape[1], shape[2]*shape[0] ) )
+    return new_data
 
 
 def classification_param_study( data ):
     """data[user,state,time] is a numpy array"""
     accuracy = eval_param_study( data )
-    analyze_param_study( accuracy )
+    model_params = analyze_param_study( accuracy )
+    
+    # now test results using the best one-versus-all classifiers together
+    confusion_matrix = test_classifier( model_params, data )
+
+
+def test_classifier( model_params, data ):
+    
 
 
 def eval_param_study( data,
@@ -162,12 +185,8 @@ def eval_param_study( data,
     
     # our figure of merit if the accuracy of the derived clasifier, which will
     # be recorded for each of the combinations of the above parameters:
-    params = zeros( [ len(users), len(states),
-                        len(domains), len(samples), len(scalers),
-                        len(methods) ] )
-    per_state_acc = zeros( [ len(users), len(states),
-                             len(domains), len(samples), len(scalers),
-                             len(methods), len(states) ] )
+    params = zeros( [ len(users), len(states) ].append( all_params.dims ) )
+    per_state_acc = zeros( params.shape.append( len(states) ) )
 
     # EVALUATION :
     ##############
@@ -176,21 +195,19 @@ def eval_param_study( data,
     for i in range( params.size ):
         [u,s,dm,spl,scl,m] = unravel_index( i, params.shape )
 
-        #---- break the data into samples
-        # dims are: data[ user, state, time ]
-        # break the data (along time axis) into given number of samples:
-        new_data = array( array_split( data, samples[spl], axis=2 ) )
-        # dims are new_data[sample#,user,state,time]
-        
         if( users[u] == "all-users" ):
-            # collapse users axis:
-            shape = new_data.shape
-            new_data = new_data.reshape( (shape[0]*shape[1],shape[2],shape[3]))
-        else:
-            # grab data from one user:
-            new_data = new_data[:,u]
-        # dims are: new_data[ sample#, state, time ]
+            new_data = flatten_users( data )
+            u = 0 # now there is only one "super-user" with index 0
+            spl *= len(users)-1 # number of samples is multiplied by # users
+        # grab data from one user:
+        new_data = new_data[u]
+        # dims are: new_data[ state, time ]
 
+        #---- break the data into samples
+        # break the data (along time axis) into given number of samples:
+        new_data = array( array_split( data, samples[spl], axis=1 ) )
+        # dims are new_data[sample#,state,time]
+        
         #---- apply the data scaler:
         if( scalers[scl] == "log" ):
             new_data = log( new_data )
@@ -241,7 +258,7 @@ def eval_param_study( data,
                     else:
                         test_data2 = [ [], test_data[:,i] ] # all neg
                         print " negative test %s accuracy: "%states[i],
-                    state_acc = svm_test( test_data2 )
+                    state_acc = svm_test( test_data2 )[0]
                     print state_acc
                     per_state_acc[u,s,dm,spl,scl,m,i] = state_acc
             else:
@@ -253,7 +270,8 @@ def eval_param_study( data,
 
 
 def analyze_param_study( per_state_acc ):
-    """per_state_acc[user,state,domain,#samples,scaler,method,state_tested]"""
+    """per_state_acc[user,state,domain,#samples,scaler,method,state_tested]
+    returns the parameters for the best models for each of the states"""
     # ANALYZE RESULTS:
     ##################
     # summarize the accuracies of classifying each state using a weighted
@@ -277,8 +295,10 @@ def analyze_param_study( per_state_acc ):
     # print state-matrix best results
     inter_state_accuracy = zeros( [2,len(states),len(states)] )
     for i in [0,1]:
+        model_params = []
         for s in range(len(states)):
             argmax = unravel_index( acc[i][s].argmax(), acc[i][s].shape )
+            model_params.append( argmax )
             inter_state_accuracy[i][s] = PSacc[i][s][argmax]
             print "state %d, best_params=%s" % (s,argmax)
         if i:
@@ -291,7 +311,9 @@ def analyze_param_study( per_state_acc ):
         print " using state i's best classifier."
         print inter_state_accuracy[i]
         print
-    return accuracy
+
+    # return best model params (for per-user-model)
+    return model_params
 
 
 if __name__ == "__main__":
