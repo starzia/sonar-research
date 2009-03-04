3f7a5b51481de715a1b818233318b803c569058b
Steve Tarzia <steve@stevetarzia.com>
2009-03-04 03:46:00 +0000

    completed new parameter study scheme, which directly optimizes confusion matrices.  Note that currently the code does not deal with multiple users, this should be a simple addition.

--- ML/classify.py
+++ ML/classify.py
@@ -25,9 +25,8 @@ def zero_crossings( arr ):
     
 
 def classification_stats( elements ):
-    """calculates some statistical properties of the
-    passed element list.  This operation
-    is useful as a preprocessing step for ML tools."""
+    """calculates some statistical properties of the passed array.
+    This operation is useful as a preprocessing step for ML tools."""
     new_elements = []
     arr = array( elements )
     stats = []
@@ -50,7 +49,7 @@ def my_kill( pid ):
     try:
         kill( pid, SIGTERM )
     except:
-        "noop"
+        pass
 
 
 def svm_light_format( training_data, in_file ):
@@ -62,18 +61,13 @@ def svm_light_format( training_data, in_file ):
             f.write( "%s " % str )
             for k in range( sample.size ):
                 f.write( "%d:%f " % (k+1, sample[k]) )
-            # add statistics to end of vector
-            # I beleive that these are properly called "kernel tricks"
-            stats = classification_stats( sample )
-            for k,val in enumerate( stats ):
-                f.write( "%d:%f " % (sample.size+k+1, val) )
             f.write("\n")
     f.close()
     
 
 def svm_train( training_data,
                model_file="/tmp/svm.model", in_file="/tmp/svm.in" ):
-    """data[ pos/neg, sample_index, data ]
+    """data[ pos/neg ][ sample_index, data ]
     returns success bool.
     model_file is the primary output and in_file is a debugging output."""
     from subprocess import Popen,PIPE
@@ -98,22 +92,23 @@ def svm_train( training_data,
                                          len( training_data[1] )    ) )
     else:
         return False
-    print "classification success for training was %d percent" % (100.0*result)
+    #print "classification success for training was %d percent" %(100.0*result)
     return True
 
 
 def svm_test( test_data, model_file="/tmp/svm.model", in_file="/tmp/svm.in",
               out_file="/tmp/svm.out" ):
-    """data[ pos/neg, sample_index, data ]
+    """data[ pos/neg ][ sample_index, data ]
     returns the accuracy.
     model_file is an input and in_file is a debugging output."""
     from subprocess import Popen,PIPE
     import re
     from threading import Timer
+    from string import rstrip
 
     # test classification model
     svm_light_format( test_data, in_file )
-    p= Popen("svm_classify %s %s" %(in_file,model_file,out_file),
+    p= Popen("svm_classify %s %s %s" %(in_file,model_file,out_file),
              shell=True,stdout=PIPE)
     # kill the process if no results in 5 seconds
     Timer( 5, my_kill, [p.pid] ).start()
@@ -125,13 +120,22 @@ def svm_test( test_data, model_file="/tmp/svm.model", in_file="/tmp/svm.in",
         f = open( out_file, 'r' )
         results = f.readlines() # this will be a float for each test vec
         f.close()
-        return [ accuracy, results ]
+        # remove trailing newline
+        for r in range(len(results)): results[r] = float(rstrip( results[r] ))
+        return [ accuracy, array(results) ]
     else:
         return [ -0.000001, [] ] # store nonsense value if svm failed
 
 
 # DEFINE PARAMETERS :
 #####################
+# training_frac is fraction of data to use as training
+training_frac=0.5
+# training_slice is which slice of size training_frac to use for
+# training.  Eg, 0 means to use the first portion of data, 1 means to use
+# the second, etc.
+training_slice=0
+
 # we will run clasification for each user plus for a combination of all:
 users = range( 20 )
 users.append( "all-users" )
@@ -163,113 +167,136 @@ def flatten_users( data ):
 
 def classification_param_study( data ):
     """data[user,state,time] is a numpy array"""
-    accuracy = eval_param_study( data )
-    model_params = analyze_param_study( accuracy )
-    
-    # now test results using the best one-versus-all classifiers together
-    confusion_matrix = test_classifier( model_params, data )
+    u = 0 # TODO: hard-code user for now
+    quality = zeros( all_params_dims )
+    for i in range( quality.size ):
+        model_params_i = unravel_index( i, all_params_dims )
+        print "testing parameters: %s" % [model_params_i]
+        confusion_matrix = test_classifier( model_params_i, data[u] )
+        print "confusion matrix:"
+        print confusion_matrix
+        print
+        quality[model_params_i] = eval_conf_matrix( confusion_matrix )
+
+    best_params = quality.argmax()
+    # print best conf matrix
+    print test_classifier( best_params, data[u] )
 
 
 def test_classifier( model_params, data ):
+    """returns a confusion matrix"""
+    #--- preprocess
+    [ training_data, test_data ] = preprocess( data, model_params )
+    #--- break into positive and negative examples for each state classifier
+    training_vectors = make_pos_neg_vectors( training_data )
+    #--- build models
+    model_filenames = []
+    for s in range( len(states) ):
+        model_filenames.append( "/tmp/svm_model%d"%s )
+        svm_train( training_vectors[s], model_filenames[s] )
+    #--- run test vectors through each model
+    num_vectors_per_state = samples[ model_params[1] ] * (1-training_frac)
+    results = zeros( [ len(states), len(states), num_vectors_per_state ] )
+    for s_model in range( len(states) ):
+        for s_actual in range( len(states) ):
+            # the svm model assigns a numeric value \in [-1,1] to each vector
+            results[s_model,s_actual] = svm_test( [test_data[:,s_actual,:],[]],
+                                                  model_filenames[s_model] )[1]
+
+    #--- classify, ie choose the best model for each vector
+    classification = results.argmax( axis=0 ) # dim: class[s_actual,vec_i]
+    confusion_matrix = zeros( [ len(states), len(states) ] )
+    for s_model in range( len(states) ):
+        confusion_matrix[s_model] = ( classification == s_model ).sum(axis=1)
+    confusion_matrix /= num_vectors_per_state # normalize to [0,1]
+    return confusion_matrix
+
+
+def eval_conf_matrix( confusion_matrix ):
+    """returns a quality metric for a given confusion matrix"""
+    dim = confusion_matrix.shape[0]
+    quality = 0
+    for i in range( dim ):
+        for j in range( dim ):
+            if( i == j ):
+                # reward true positives
+                quality += confusion_matrix[i,j]
+    return quality
+
+
+def preprocess( data, model_params ):
+    """return a list of classification vectors based on the passed parameters
+    for both training and testing sets: t*_data[sample#,state,feature_index]
     
-
-
-def eval_param_study( data,
-                      training_frac = 0.5, training_slice=0 ):
-    """data[user,state,time] is a numpy array
-    training_frac is fraction of data to use as training
-    training_slice is which slice of size training_frac to use for
-    training.  Eg, 0 means to use the first portion of data, 1 means to use
-    the second, etc."""
+    data[state,time] is a numpy array"""
     from numpy.fft import fft
+    [dm,spl,scl,m] = model_params
+
+    #---- break the data into samples
+    # break the data (along time axis) into given number of samples:
+    new_data = array( array_split( data, samples[spl], axis=1 ) )
+    # dims are new_data[sample#,state,time]
     
-    # our figure of merit if the accuracy of the derived clasifier, which will
-    # be recorded for each of the combinations of the above parameters:
-    params = zeros( [ len(users), len(states) ].append( all_params.dims ) )
-    per_state_acc = zeros( params.shape.append( len(states) ) )
+    #---- apply the data scaler:
+    if( scalers[scl] == "log" ):
+        new_data = log( new_data )
+    elif( scalers[scl] == "exp" ):
+        new_data = exp( new_data )
+    elif( scalers[scl] == "square" ):
+        new_data = new_data**2
+    elif( scalers[scl] == "sqrt" ):
+        new_data = new_data**0.5
 
-    # EVALUATION :
-    ##############
-    # For each of the combinations of parameters we will generate a list of
-    # classification vectors (samples)
-    for i in range( params.size ):
-        [u,s,dm,spl,scl,m] = unravel_index( i, params.shape )
+    #---- apply the domain transform
+    if( domains[dm] == "freq" ):
+        new_data = fft( new_data )
 
-        if( users[u] == "all-users" ):
-            new_data = flatten_users( data )
-            u = 0 # now there is only one "super-user" with index 0
-            spl *= len(users)-1 # number of samples is multiplied by # users
-        # grab data from one user:
-        new_data = new_data[u]
-        # dims are: new_data[ state, time ]
+    #---- add statistics to end of vector
+    # I beleive that these are properly called "kernel tricks"
+    num_stats = len( classification_stats( new_data[0,0] ) )
+    stats = zeros( [ samples[spl], len(states), num_stats ] )
+    for i in range(spl):
+        for j in range(len(states)):
+            stats[i,j] = classification_stats( new_data[i,j] )
+    new_data = concatenate( [new_data, stats], 2 ) # axis=2
 
-        #---- break the data into samples
-        # break the data (along time axis) into given number of samples:
-        new_data = array( array_split( data, samples[spl], axis=1 ) )
-        # dims are new_data[sample#,state,time]
-        
-        #---- apply the data scaler:
-        if( scalers[scl] == "log" ):
-            new_data = log( new_data )
-        elif( scalers[scl] == "exp" ):
-            new_data = exp( new_data )
-        elif( scalers[scl] == "square" ):
-            new_data = new_data**2
-        elif( scalers[scl] == "sqrt" ):
-            new_data = new_data**0.5
-
-        #---- apply the domain transform
-        if( domains[dm] == "freq" ):
-            new_data = fft( new_data )
-
-        #---- break into training and test fractions:
-        # training data may be taken from middle, so we have to split the data
-        # and rejoin the left and right test segments
-        split1_index = ceil( training_frac * training_slice * samples[spl] )
-        split2_index = ceil( training_frac *(training_slice+1) * samples[spl] )
-        [test1_data,training_data,test2_data] = array_split( new_data,
+    #---- break into training and test fractions:
+    # training data may be taken from middle, so we have to split the data
+    # and rejoin the left and right test segments
+    split1_index = ceil( training_frac * training_slice * samples[spl] )
+    split2_index = ceil( training_frac *(training_slice+1) * samples[spl] )
+    [test1_data,training_data,test2_data] = array_split( new_data,
                                             [split1_index,split2_index],axis=0)
-        if test1_data.size == 0:
-            test_data = test2_data
-        elif test2_data.size == 0:
-            test_data = test1_data
-        else:
-            test_data = vstack( [test1_data,test2_data] )
+    if test1_data.size == 0:
+        test_data = test2_data
+    elif test2_data.size == 0:
+        test_data = test1_data
+    else:
+        test_data = vstack( [test1_data, test2_data] )
+    # dims are t*_data[sample#,state,feature_index]
+    return [ training_data, test_data ]
+    
 
-        #---- label training_data as either positive or negative examples:
-        pos_examples = training_data[:,s]
+def make_pos_neg_vectors( data ):
+    """return ret[ state ][pos/neg ][ sample_index, data ]
+    arg data[ sample_index, state, data ]"""
+    ret = []
+    for s in range(len(states)):
+        ret.append([])
+        # for this state, label data as either positive or negative examples:
+        pos_examples = data[:,s]   # positive examples
         neg_examples = []
-        for i in range( len(states) ):
+        for i in range( len(states) ): # negative examples, from all others
             if( i != s ):
-                neg_examples.append( training_data[:,i] )
+                neg_examples.append( data[:,i] )
         neg_examples = vstack( neg_examples )
-        # dims are: *_examples[ sample#, time ]
-        training_data = [ pos_examples, neg_examples ]
-
-        #---- now run ML algorithm
-        print "user=%s state=%s scaler=%s samples=%03d domain=%s method=%s" % (users[u],states[s],scalers[scl],samples[spl],domains[dm],methods[m]) 
-        if( methods[m] == "svm" ):
-            if( svm_train( training_data ) ):
-                # for all states other than the one we are currently detecting:
-                for i in range( len( states ) ):
-                    if( i == s ):
-                        test_data2 = [ test_data[:,i], [] ] # all pos
-                        print " positive test accuracy: ",
-                    else:
-                        test_data2 = [ [], test_data[:,i] ] # all neg
-                        print " negative test %s accuracy: "%states[i],
-                    state_acc = svm_test( test_data2 )[0]
-                    print state_acc
-                    per_state_acc[u,s,dm,spl,scl,m,i] = state_acc
-            else:
-                "noop. we get here is training fails."
-        elif( methods[m] == "neural net" ):
-            "noop"
-        print
-    return per_state_acc
+        # dims are: *_examples[ sample#, data ]
+        ret[s] = [ pos_examples, neg_examples ]
+    return ret
 
 
-def analyze_param_study( per_state_acc ):
+
+def OLD_analyze_param_study( per_state_acc ):
     """per_state_acc[user,state,domain,#samples,scaler,method,state_tested]
     returns the parameters for the best models for each of the states"""
     # ANALYZE RESULTS:
