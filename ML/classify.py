2323d4fa8e9458a71b5001d21c40bfa543ed7965
Steve Tarzia <steve@stevetarzia.com>
2009-04-13 22:05:10 +0000

    switched to CDF-based classifier

--- ML/classify.py
+++ ML/classify.py
@@ -5,6 +5,12 @@
 
 from numpy import *
 
+def assert_file_exists( filename ):
+    from os.path import exists
+    if( not exists( filename ) ):
+        raise "file %s does not exist!"%filename
+
+
 def read_txt_file( filename ):
     """reads a file which has a single value on each line and returns
     as a list."""
@@ -67,9 +73,11 @@ def svm_light_format( training_data, in_file ):
 
 def svm_train( training_data,
                model_file="/tmp/svm.model", in_file="/tmp/svm.in" ):
-    """data[ pos/neg ][ sample_index, data ]
-    returns success bool.
-    model_file is the primary output and in_file is a debugging output."""
+    """
+    @param training_data[ pos/neg ][ sample_index, data ]
+    @param model_file is the primary output and
+    @param in_file is a debugging output.
+    @return success bool."""
     from subprocess import Popen,PIPE
     import re
     from threading import Timer
@@ -79,10 +87,10 @@ def svm_train( training_data,
     # weight positive examples by a factor so that pos and neg accuracy
     # have the same cumulative weight when training
     pos_weight = len(training_data[1]) / float(len(training_data[0]))
-    p = Popen("svm_learn -j %f %s %s" % (pos_weight,in_file,model_file),
+    p = Popen("svm_learn -t 0 -j %f %s %s" % (pos_weight,in_file,model_file),
               shell=True, stdout=PIPE)
-    # kill the process if no results in 5 seconds
-    Timer( 5, my_kill, [p.pid] ).start()
+    # kill the process if no results in 10 seconds
+    Timer( 10, my_kill, [p.pid] ).start()
     output = p.communicate()[0]
     # parse out results
     match = re.search( "\((.*) misclassified,", output )
@@ -90,21 +98,24 @@ def svm_train( training_data,
         misclassified = float( match.group(1) )
         result = 1 - ( misclassified / ( len( training_data[0] ) +
                                          len( training_data[1] )    ) )
+        assert_file_exists( model_file )
     else:
-        return False
+        raise "timeoutError"
     #print "classification success for training was %d percent" %(100.0*result)
-    return True
 
 
 def svm_test( test_data, model_file="/tmp/svm.model", in_file="/tmp/svm.in",
               out_file="/tmp/svm.out" ):
-    """data[ pos/neg ][ sample_index, data ]
-    returns the accuracy.
-    model_file is an input and in_file is a debugging output."""
+    """@param test_data[ pos/neg ][ sample_index, data ]
+    @param model_file is an input
+    @param in_file is a debugging output.
+    @return [accuracy, array(results)]."""
+
     from subprocess import Popen,PIPE
     import re
     from threading import Timer
     from string import rstrip
+    assert_file_exists( model_file )
 
     # test classification model
     svm_light_format( test_data, in_file )
@@ -124,6 +135,7 @@ def svm_test( test_data, model_file="/tmp/svm.model", in_file="/tmp/svm.in",
         for r in range(len(results)): results[r] = float(rstrip( results[r] ))
         return [ accuracy, array(results) ]
     else:
+        print "svm_test: failure!"
         return [ -0.000001, [] ] # store nonsense value if svm failed
 
 
@@ -158,6 +170,67 @@ all_params_dims = []
 for i in all_params: all_params_dims.append( len(i) )
 
 
+class stateClassifier:
+    """a state classifier for time series data"""
+    count = 0 # number of object instances, for uniquely naming modelfiles
+
+    def __init__( self, training_data ):
+        """constructor trains a classification model.
+        @param training_data[ sample_index, state, data ]"""
+        self.modelfiles = []
+        self.training_CDFs = []
+        stateClassifier.count += 1
+        
+        #--- break into positive and neg. examples for each state classifier
+        training_vectors = make_pos_neg_vectors( training_data )
+        #--- build models for each state
+        filename_base = "/tmp/model.%d." % stateClassifier.count
+        for s in range( len(states) ):
+            self.modelfiles.append( "%s%d" % (filename_base,s) )
+            svm_train( training_vectors[s], self.modelfiles[s] )
+        #--- build CDF of distance value for training data under each model
+        shape = training_data.shape
+        # run data for all states through new SVMs
+        for s_model in range( len(states) ):
+            # below, concatenate data for all states into one big set
+            results = svm_test( [training_data.reshape((shape[0]*shape[1],
+                                                        shape[2] )), [] ],
+                                self.modelfiles[s_model] )[1]
+            # make (an inverse?) training_CDF from results by sorting data
+            self.training_CDFs.append( sort(results) )
+
+    def classify( self, test_data ):
+        """For each test vector, classification done by:
+        - running vector through each state's SVM model, returning a 'distance'
+        - choosing the state whose distance value is lowest as a percentile in
+           the training_CDF.  This effectively normalizes the model distances.
+
+        @param test_data[sample_index,data]
+        @return state index"""
+        # run data through SVMs
+        percentile = []
+        for s_model in range( len(states) ):
+            # get raw SVM results (distances)
+            results = svm_test( [test_data, [] ], self.modelfiles[s_model] )[1]
+            # convert distances into percentile using training_CDF
+            rp = [] # results percentiles
+            for r in results:
+                rp.append( searchsorted( self.training_CDFs[s_model], r ) )
+            percentile.append( rp )
+        percentile = array( percentile )
+        # for each data vector, choose state model which produced the highest
+        # percentile value
+        return percentile.argmax( axis=0 )
+    
+    def dealloc( self ):
+        """destructor"""
+        import os
+        # delete model files
+        for m in self.modelfiles:
+            os.remove( m )
+# end class stateClassifier
+
+
 def flatten_users( data ):
     """data[user,state,time] is a numpy array
     combines data from all users into one super-user"""
@@ -178,8 +251,13 @@ def classification_param_study( data ):
         # confusion matrix will calculated for each user, then totalled
         confusion_matrices = zeros( [ len(users), len(states), len(states) ] )
         for u in users:
-            confusion_matrices[u] = classifier_confusion( model_params_i,
-                                                          data[u] )
+            try:
+                confusion_matrices[u] = classifier_confusion( model_params_i,
+                                                              data[u] )
+            except:
+                # if classification fails, we will actually have a zero-filled
+                # confusion matrix, so N.B. column sums may be less than one 
+                pass
         # total confusion matrix
         confusion_matrix = confusion_matrices.mean(axis=0)
         print "confusion matrix:"
@@ -220,7 +298,7 @@ def run_svm_state_models( model_params, data ):
             # the svm model assigns a numeric value \in [-1,1] to each vector
             results[s_model,s_actual] = svm_test( [test_data[:,s_actual,:],[]],
                                                   model_filenames[s_model] )[1]
-    return results
+            return results
 
 
 def classifier_confusion( model_params, data ):
@@ -229,13 +307,18 @@ def classifier_confusion( model_params, data ):
 
     Classification is done by running data agains svm models for each state
     and choosing the state whose model returned the highest confidence value"""
-    results = run_svm_state_models( model_params, data )
-    #--- classify, ie choose the best model for each vector
-    classification = results.argmax( axis=0 ) # dim: class[s_actual,vec_i]
+    #- preprocess
+    [ training_data, test_data ] = preprocess( data, model_params )
+    #- train classifier
+    classifier = stateClassifier( training_data )
+    #- for \E actual state: classify, ie choose the best model for each vector
     confusion_matrix = zeros( [ len(states), len(states) ] )
-    for s_model in range( len(states) ):
-        confusion_matrix[s_model] = ( classification == s_model ).sum(axis=1)
-    confusion_matrix /= num_vectors_per_state # normalize to [0,1]
+    for s_actual in range( len(states) ):
+        classification = classifier.classify( test_data[s_actual] )
+        confusion_matrix[:,s_actual] = histogram( classification,
+                                                  range(len(states)),
+                                                  normed=True )[0]
+    classifier.dealloc()
     return confusion_matrix
 
 
@@ -250,10 +333,11 @@ def eval_conf_matrix( confusion_matrix ):
 
 
 def preprocess( data, model_params ):
-    """return a list of classification vectors based on the passed parameters
-    for both training and testing sets: t*_data[sample#,state,feature_index]
-    
-    data[state,time] is a numpy array"""
+    """    
+    @param data[state,time] is a numpy array
+    @return [ training_data[sample#,state,feature_index], test_data[...] ]
+      a list of classification vectors based on the passed parameters
+      for both training and testing sets"""
     from numpy.fft import fft
     [dm,spl,scl,sts,m] = model_params
 
